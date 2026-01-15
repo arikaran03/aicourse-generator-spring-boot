1c15d3164b857a5945fe34065af5c11669e6d2f2
arikaran03 <arikaran258@gmail.com>
2026-01-15 18:36:17 +0530

    added user table structure in the DB package

---
+++ src/main/java/com/aicourse/db/USERS.java
@@ -0,0 +1,15 @@
+package com.aicourse.db;
+
+public class USERS {
+
+    private USERS() {}
+
+    public static final String TABLE = "users";
+
+    public static final String ID = "id";
+    public static final String USERNAME = "username";
+    public static final String PASSWORD = "password";
+    public static final String ROLES = "roles";
+    public static final String CREATED_AT = "created_at";
+    public static final String UPDATED_AT = "updated_at";
+}
