dab45163423464bfbe2db9db080a571c10f8d38f
arikaran03 <arikaran258@gmail.com>
2026-01-17 15:49:55 +0530

    Temporarily disable scheduler class

--- src/main/java/com/aicourse/scheduler/LessonEnrichmentScheduler.java
+++ src/main/java/com/aicourse/scheduler/LessonEnrichmentScheduler.java
@@ -4,22 +4,22 @@ import com.aicourse.service.courses.LessonService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-@Component
-public class LessonEnrichmentScheduler {
-
-    private final LessonService lessonService;
-
-    public LessonEnrichmentScheduler(LessonService lessonService) {
-        this.lessonService = lessonService;
-    }
-
-    @Scheduled(fixedRate = 60_000)
-    public void enrichLessonsJob() {
-        System.out.println(
-                "[SCHEDULER] Triggered on thread: " +
-                        Thread.currentThread().getName()
-        );
-
+//@Component
+//public class LessonEnrichmentScheduler {
+//
+//    private final LessonService lessonService;
+//
+//    public LessonEnrichmentScheduler(LessonService lessonService) {
+//        this.lessonService = lessonService;
+//    }
+//
+//    @Scheduled(fixedRate = 60_000)
+//    public void enrichLessonsJob() {
+//        System.out.println(
+//                "[SCHEDULER] Triggered on thread: " +
+//                        Thread.currentThread().getName()
+//        );
+//
 //        lessonService.enrichPendingLessonsLimited(); // Temporarily commented out due to a high number of API calls when multiple courses are being created
-    }
-}
+//    }
+//}
