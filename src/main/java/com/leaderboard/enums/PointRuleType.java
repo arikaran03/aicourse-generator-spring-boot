8b9f8dfd5b965c3d1cbac6f3871ba566fbca84f7
arikaran03 <arikaran258@gmail.com>
2026-02-06 00:55:57 +0530

    Introduced PointRuleType enum

--- src/main/java/com/leaderboard/enums/PointRuleType.java
+++ src/main/java/com/leaderboard/enums/PointRuleType.java
@@ -1,4 +1,18 @@
 package com.leaderboard.enums;
 
 public enum PointRuleType {
+    COURSE_COMPLETION("course_completion"),
+    LESSON_COMPLETION("lesson_completion"),
+    DAILY_LOGIN("daily_login"),
+    STREAK_BONUS("streak_bonus");
+
+    private final String value;
+
+    PointRuleType(String value) {
+        this.value = value;
+    }
+
+    public String getValue(){
+        return value; 
+    }
 }
