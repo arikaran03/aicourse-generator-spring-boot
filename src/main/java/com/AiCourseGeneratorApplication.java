037c26cfb8b60a5fe2e0bf81c3dc78aa78357b88
arikaran03 <arikaran258@gmail.com>
2026-02-26 00:07:23 +0530

    package movement mainclass from com.aicourse to com
    Signed-off-by: arikaran03 <arikaran258@gmail.com>

--- src/main/java/com/AiCourseGeneratorApplication.java
+++ src/main/java/com/AiCourseGeneratorApplication.java
@@ -1,4 +1,4 @@
-package com.aicourse;
+package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,6 +6,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+//@ComponentScan(basePackages = {"com.aicourse", "com.leaderboard"})
 public class AiCourseGeneratorApplication {
 
 	public static void main(String[] args) {
